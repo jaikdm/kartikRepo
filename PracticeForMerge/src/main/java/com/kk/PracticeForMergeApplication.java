e6132100d430b1d66bb3b1eb023fe13d2afa9c09
jaikdm <jaikdm95@gmail.com>
2022-08-24 15:40:46 +0530

    kk

---
+++ PracticeForMerge/src/main/java/com/kk/PracticeForMergeApplication.java
@@ -0,0 +1,13 @@
+package com.kk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeForMergeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeForMergeApplication.class, args);
+	}
+
+}
---
+++ PracticeForMerge/src/main/java/com/kk/controller/KartikController.java
@@ -0,0 +1,16 @@
+package com.kk.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class KartikController {
+
+    @GetMapping
+    public String sayName(){
+        return "I am Kartik Kadam";
+    }
+}
